8deb28528a01f5bc9956a3737100512dfa540388
ARBhosale <anirsk@gmail.com>
2018-05-02 20:46:19 -0400

    added player not found exception, throwing while creating game

--- othello-new-server/othello-server/src/main/java/gmu/isa681/project/othelloserver/rest/PlayingResource.java
+++ othello-new-server/othello-server/src/main/java/gmu/isa681/project/othelloserver/rest/PlayingResource.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -16,9 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import gmu.isa681.project.othelloserver.convertor.GameEntityToPlayingResponseConverter;
 import gmu.isa681.project.othelloserver.entity.GameEntity;
 import gmu.isa681.project.othelloserver.entity.PlayerEntity;
+import gmu.isa681.project.othelloserver.exception.PlayerNotFoundException;
 import gmu.isa681.project.othelloserver.model.request.PlayingRequest;
 import gmu.isa681.project.othelloserver.model.request.game.play.NewGameRequest;
 import gmu.isa681.project.othelloserver.model.response.game.PlayingResponse;
@@ -59,6 +58,10 @@ public class PlayingResource {
 
 	@RequestMapping(path = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<PlayingResponse> createNewGame(@RequestBody NewGameRequest newGameRequest) {
+		Optional<PlayerEntity> player = playerRepository.findById(newGameRequest.getRequestSenderPlayerId());
+		if (!player.isPresent()) {
+			throw new PlayerNotFoundException(newGameRequest.getRequestSenderPlayerId().toString());
+		}
 		GameEntity game = conversionService.convert(newGameRequest, GameEntity.class);
 		gameRepository.save(game);
 		PlayingResponse playingResponse = conversionService.convert(game, PlayingResponse.class);
